8763c7502ab8cec89b25a14b7160e2176650401f
Cedric <crubbertire@rogers.com>
2025-09-24 22:59:34 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')();
+const username = prompt('What is your name? ');
+
+const inventory = {
+  money: 0,
+  luck: 1,
+  abilities: [],
+}
+
+const abilities = {
+  luckUp: function() {
+
+  },
+
+}
+
+const spinWheel = () => {
+  
+}
+
+
+
+
+
+
